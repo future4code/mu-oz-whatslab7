f5f5b9399c4237e7f7d6d34f4be4151cf754f6a6
Clara Meirelles <clara_meirelles@hotmail.com>
2021-05-08 22:22:54 -0300

    whatslab funcionando, mas não bonito

---
+++ src/components/ContainerPrincipal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderUsuário = styled.div`
+    background-color: gray;
+    width: 20vw;
+`
+
+export class ContainerPrincipal extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <HeaderUsuário>
+                    
+                </HeaderUsuário>
+
+            </div>
+        )
+    }
+}
+
